cee2da528f70e083d5a8efb859e97fe617d33f56
Gerardo <gerarllano@gmail.com>
2015-05-15 15:31:57 +0200

    edited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This assignment is pretty dificult to be understood theoretically. Despite of that, the
+## example provided by DanieleP makes it lot easier: https://github.com/DanieleP/PA2-clarifying_instructions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xinv <- NULL # This is where the result of inversion is stored
+  # This is used to set a matrix to object created by makecacheMatrix function
+  
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL # It initialises xinv to null
+  }
+  
+  get <- function() x # It returns the input matrix
+  setInv <- function(inv) xinv <<- inv # It sets the inversed matrix
+  getInv <- function() xinv # It returns the inversed matrix
+  
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv) # It returns a list that contains these functions. This, this
+  # functions can be used conviniently.
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv() # It gets the inversed matrix from x. I'll be null if uncalculated.
+  if(!is.null(m)) { # if the inversion result is there
+    message("getting cached data")
+    return(m) # it returns the calculated inversion
+  }
+  data <- x$get() # if not, we do x$get to get the matrix object
+  m <- solve(data) # then we solve it
+  x$setInv(m) # and then we set it to the object
+  m # Then, we return the solved result
 }
